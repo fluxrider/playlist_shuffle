767464e3f16e827138c2ab34a08c653fb618acd1
fluxrider <doompilot@gmail.com>
2018-10-05 10:41:03 -0400

    move files in /res and /src

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 set -e
 
-gcc stats.c -ldl -lm
-gcc -shared -fPIC -o order.so order.c
-gcc -shared -fPIC -o random.so random.c -lbsd
-gcc -shared -fPIC -o shuffle.so shuffle.c -lbsd
-gcc -shared -fPIC -o disjoint.so disjoint.c -lbsd
-gcc -shared -fPIC -o split.so split.c -lbsd
-gcc -shared -fPIC -o split_r.so split_r.c -lbsd
-gcc -shared -fPIC -o overlap.so overlap.c -lbsd
+gcc src/stats.c -ldl -lm
+gcc -shared -fPIC -o order.so src/order.c
+gcc -shared -fPIC -o random.so src/random.c -lbsd
+gcc -shared -fPIC -o shuffle.so src/shuffle.c -lbsd
+gcc -shared -fPIC -o disjoint.so src/disjoint.c -lbsd
+gcc -shared -fPIC -o split.so src/split.c -lbsd
+gcc -shared -fPIC -o split_r.so src/split_r.c -lbsd
+gcc -shared -fPIC -o overlap.so src/overlap.c -lbsd
